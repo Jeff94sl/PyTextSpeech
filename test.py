21daa02ec8a198d4c4eaa8609f4b9bdb09d08bad
Jeff94sl <jeffrynsl@hotmail.es>
2022-01-08 23:30:54 -0600

    actualizacion

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from engines.document import Document
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True,True)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
